b0c33ad389dc76875a071886af06931421468bf8
unknown <oseiasc2@gmail.com>
2024-02-01 11:52:17 -0300

    feat: implemented dashboard

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,37 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet, useLocation } from "react-router-dom"
 import Logo from "/logo-kedis.svg"
+import Button from "@mui/material/Button"
 
 export default function Layout(){
+    const location = useLocation()
+    const path = location.pathname
+
+    const links = menuData.map((item) => {
+        return(
+            <Link to={item.path} key={item.id}>
+                <Button 
+                    style={style.menu.link} 
+                    variant="text"
+                    sx={{ 
+                        bgcolor: path === item.path ? "#26282B" : null,
+                        color: path === item.path ? "#D3D3D3" : "#959595"
+                    }}
+                >
+                    {item.text}
+                </Button>
+            </Link>
+        )
+    })
+
     return(
         <section style={style.container}>
             <div style={style.menu.box}>
                 <img className="logo" src={Logo} style={style.menu.img} alt="Logo Kedis" />
+                <div>{links}</div>
             </div>
             <div>
-                <header></header>
-                <div><Outlet /></div>
+                <header style={style.header}></header>
+                <div style={style.content}><Outlet /></div>
             </div>
         </section>
     )
@@ -18,16 +40,67 @@ export default function Layout(){
 const style = {
     container: {
         display: "flex",
-        justifyContent: "start"
+        justifyContent: "start",
+        height: "100vh",
+    },
+    content: {
+        padding: 32
+    },
+    header: {
+        borderBottom: "1px solid #40464D",
+        height: 50,
+        width: "calc(100vw - 200px)"
     },
     menu: {
         box: {
-            borderWidthRight: 1,
-            borderColor: "red" 
+            borderRight: "1px solid #40464D", 
+            width: 200,
         },
         img: {
             width: 120,
             height: "auto"
+        }, 
+        link: {
+            width: "90%",
+            marginLeft: "5%",
+            marginRight: "5%",
+            textDecoration: "none",
+            fontWeight: 500,
+            paddingBottom: 0,
+            justifyContent: "start",
+            paddingTop: 3,
+            marginBottom: 4,
+            paddingLeft: 26,
+            borderRadius: 4,
+            textTransform: "capitalize" as const
         }
     }
 }
+
+const menuData = [
+    {
+        id: 1, 
+        path: "/",
+        text: "Dashboard"
+    },
+    {
+        id: 2, 
+        path: "/simulados",
+        text: "Simulados"
+    },
+    {
+        id: 3, 
+        path: "/resultados",
+        text: "Resultados"
+    },
+    {
+        id: 4, 
+        path: "/planos",
+        text: "Planos"
+    },
+    {
+        id: 5, 
+        path: "/conta",
+        text: "Conta"
+    },
+]
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,55 @@
 import { Button } from "@mui/material";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fecthUser, initialUserState } from "../redux/user.slice";
+import { RootState } from "../redux/store";
 
 export default function Dashboard(){
     const dispatch = useDispatch()
+    const user = useSelector((state:RootState) => state.user.user)
 
     return(
         <section>
             <h1>Dashboard</h1>
+            <p style={s.welcome}>Bem vindo, {user.firstName}.</p>
+            <div style={s.panel.container}>
+                <div style={s.panel.header}>
+                    <p style={{paddingLeft: 20, fontSize: 14, width: 298.7}}>Últimos simulados</p>
+                    <p style={{fontSize: 14}}>Certas</p>
+                </div>
+            <div style={{display: "flex"}}>
+                <div style={{padding: 20}}>
+                    <p style={{fontSize: 12}}>Fevereiro</p>
+                    <p style={{fontSize: 40, position: "relative", bottom: 10}}>06</p>
+                </div>
+                <p style={{padding: 20, fontSize: 18, width: 170}}>Simulado Aws Cloud Pratictioner #1</p>
+            </div>
+            </div>
             <Button onClick={() => dispatch(fecthUser(initialUserState))}>Logout</Button>
         </section>
     )
+}
+
+const s = {
+    welcome: {
+        fontWeight: 200,
+        position: "relative" as const,
+        bottom: 8,
+        color: "#959595",
+        fontSize: 14,
+        paddingBottom: 20
+    },
+    panel: {
+        container: {
+            border: "1px solid #524E4E",
+            height: 200,
+            width: 600,
+            borderRadius: 10
+        },
+        header: {
+            borderBottom: "1px solid #524E4E",
+            height: 35,
+            display: "flex",
+            alignItems: "center"     
+        }
+    }
 }
